62b2f6876c73c5c0b1cef2d95e697cc52079378b
岡田玄哉 <g.okada@trifort.jp>
2016-09-05 23:04:04 +0900

    DB内サーチを追加

--- twitter/DB.php
+++ twitter/DB.php
@@ -25,6 +25,18 @@ Class DB {
     /**
     * twitterユーザの登録
     */
+    public function queryWord($word) {
+
+        $prepare = $this->instance->prepare("SELECT * FROM `t_tweet` WHERE `text` LIKE :word ORDER BY `t_tweet`.`retweet_count` DESC");
+        $prepare->bindValue(':word', "%{$word}%", PDO::PARAM_STR);
+
+        $prepare->execute();
+        return $prepare->fetchAll();
+    }
+
+    /**
+    * twitterユーザの登録
+    */
     public function registerTwitterUser($user) {
 
         $sql = '';
@@ -56,17 +68,17 @@ VALUES (:user_id,
 SQL;
 
         $prepare = $this->instance->prepare($sql);
-        $prepare->bindParam(':user_id', $user['user_id'], PDO::PARAM_INT);
-        $prepare->bindParam(':name', $user['name']);
-        $prepare->bindParam(':screen_name', $user['screen_name']);
-        $prepare->bindParam(':location', $user['location']);
-        $prepare->bindParam(':description', $user['description']);
-        $prepare->bindParam(':url', $user['url']);
-        $prepare->bindParam(':profile_image_url_https', $user['profile_image_url_https']);
-        $prepare->bindParam(':followers_count', $user['followers_count'], PDO::PARAM_INT);
-        $prepare->bindParam(':friends_count', $user['friends_count'], PDO::PARAM_INT);
-        $prepare->bindParam(':listed_count', $user['listed_count'], PDO::PARAM_INT);
-        $prepare->bindParam(':created_at', $user['created_at']);
+        $prepare->bindValue(':user_id', $user['user_id'], PDO::PARAM_INT);
+        $prepare->bindValue(':name', $user['name']);
+        $prepare->bindValue(':screen_name', $user['screen_name']);
+        $prepare->bindValue(':location', $user['location']);
+        $prepare->bindValue(':description', $user['description']);
+        $prepare->bindValue(':url', $user['url']);
+        $prepare->bindValue(':profile_image_url_https', $user['profile_image_url_https']);
+        $prepare->bindValue(':followers_count', $user['followers_count'], PDO::PARAM_INT);
+        $prepare->bindValue(':friends_count', $user['friends_count'], PDO::PARAM_INT);
+        $prepare->bindValue(':listed_count', $user['listed_count'], PDO::PARAM_INT);
+        $prepare->bindValue(':created_at', $user['created_at']);
 
         $prepare->execute();
     }
@@ -91,13 +103,13 @@ NOW()
 SQL2;
 
         $prepare = $this->instance->prepare($sql);
-        $prepare->bindParam(':id', $tweet['id'], PDO::PARAM_INT);
-        $prepare->bindParam(':user_id', $tweet['user']['user_id'], PDO::PARAM_INT);
-        $prepare->bindParam(':retweet_count', $tweet['retweet_count'], PDO::PARAM_INT);
-        $prepare->bindParam(':favorite_count', $tweet['favorite_count'], PDO::PARAM_INT);
-        $prepare->bindParam(':text', $tweet['text']);
-        $prepare->bindParam(':media', $tweet['media']);
-        $prepare->bindParam(':created_at', $tweet['created_at']);
+        $prepare->bindValue(':id', $tweet['id'], PDO::PARAM_INT);
+        $prepare->bindValue(':user_id', $tweet['user']['user_id'], PDO::PARAM_INT);
+        $prepare->bindValue(':retweet_count', $tweet['retweet_count'], PDO::PARAM_INT);
+        $prepare->bindValue(':favorite_count', $tweet['favorite_count'], PDO::PARAM_INT);
+        $prepare->bindValue(':text', $tweet['text']);
+        $prepare->bindValue(':media', $tweet['media']);
+        $prepare->bindValue(':created_at', $tweet['created_at']);
         $prepare->execute();
     }
 
